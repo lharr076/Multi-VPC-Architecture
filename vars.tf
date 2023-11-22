f2e2878f4470a6203d7beec6d578651db596066c
Larry <74632402+lharr076@users.noreply.github.com>
2023-11-22 07:53:52 -0500

    Update vars.tf
    Add "aws-key" variable

--- vars.tf
+++ vars.tf
@@ -3,3 +3,9 @@ variable "region" {
     default="us-east-2"
 }
 
+variable "aws-key" {
+    description = "SSH Public Key Name Created in AWS (keys are unique per region)."
+    type = string
+    default = "US-EAST-2-KEY"
+
+}
